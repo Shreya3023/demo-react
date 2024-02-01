1bae490f47e2b2779d75ca2902261434ce0a0a1c
SHREYA PRASAD <prasadshreya2303@gmail.com>
2024-02-01 15:50:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header.js'
+import Footer from './Footer.js'
+import Facts from './Facts.js'
+function App() {
+  return(
+    <> 
+  <Header/>
+  <Facts/>
+  <Footer/>
+  </>
+  );
+          
+}
+
+export default App;
---
+++ src/Facts.js
@@ -0,0 +1,14 @@
+function Facts(){
+    const fact1 = "This page was create in the year 2024";
+    const fact2 = "This is my first react project";
+    const fact3 = "Today is Wednesday";
+return(
+    <ul>
+        <li>{fact1}</li>
+        <li>{fact2}</li>
+        <li>{fact3}</li>
+    </ul>
+);
+
+}
+export default Facts
